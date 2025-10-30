bb38ebd277ad00f257e1ad87c10c7ef6d1db1dfb
Angeline <angelinehephzibahj@gmail.com>
2025-10-30 18:05:56 +0000

    CHANGE THE LOGO (_for element <Logo>_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,9 +12,9 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" />
-      <path d="m9.5 9.5 5 5" />
-      <path d="m14.5 9.5-5 5" />
+      <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12" />
+      <path d="m7 13 5 5 5-5" />
+      <path d="m12 18 0-12" />
     </svg>
   );
 }
